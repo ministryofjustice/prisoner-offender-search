6f6f585b0e283e7964c4d67a3611a66b1afad5db
Michael Willis <michael.willis@digital.justice.gov.uk>
2020-04-21 16:18:07 +0100

    Simple Indexing

--- src/main/kotlin/uk/org/justice/digital/hmpps/prisonersearch/repository/PrisonerRepository.kt
+++ src/main/kotlin/uk/org/justice/digital/hmpps/prisonersearch/repository/PrisonerRepository.kt
@@ -2,11 +2,16 @@ package uk.org.justice.digital.hmpps.prisonersearch.repository
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.elasticsearch.annotations.Query
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import uk.org.justice.digital.hmpps.prisonersearch.model.Prisoner
+import java.time.LocalDate
 
 interface PrisonerRepository : ElasticsearchRepository<Prisoner, String> {
-    fun findByLastNameAndFirstName(lastName: String, firstName: String, pageable: Pageable?): Page<Prisoner>
+    @Query("{\"multi_match\": {\"query\": \"?0\", \"fields\": [\"lastName\", \"firstName\", \"prisonerId\", \"agencyId\" ], \"fuzziness\": \"AUTO\"}}")
+    fun findByKeywords(keywords: String, pageable: Pageable?): Page<Prisoner>
 
     fun findByPrisonerId(prisonerId: String): Prisoner
+
+    fun findByDateOfBirth(dateOfBirth : LocalDate, pageable: Pageable?): Page<Prisoner>
 }
