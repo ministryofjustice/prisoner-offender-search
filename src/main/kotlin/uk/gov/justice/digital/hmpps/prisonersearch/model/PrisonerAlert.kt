93ef1eecacd5b86d787a669c0fe526e600e5404b
Michael Willis <michael.willis@digital.justice.gov.uk>
2020-05-19 17:05:19 +0100

    DT-674 - Add docs an additional events to trap (#22)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/PrisonerAlert.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/PrisonerAlert.kt
@@ -1,7 +1,10 @@
 package uk.gov.justice.digital.hmpps.prisonersearch.model
 
+import io.swagger.annotations.ApiModelProperty
+
 data class PrisonerAlert (
-  val alertId: Long,
+  @ApiModelProperty(required = true, value = "Alert Type", example = "H", position = 1)
   val alertType: String,
+  @ApiModelProperty(required = true, value = "Alert Code", example = "HA", position = 2)
   val alertCode: String
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/PrisonerAlias.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/PrisonerAlias.kt
@@ -1,16 +1,28 @@
 package uk.gov.justice.digital.hmpps.prisonersearch.model
 
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.data.elasticsearch.annotations.DateFormat
 import org.springframework.data.elasticsearch.annotations.Field
 import org.springframework.data.elasticsearch.annotations.FieldType
 import java.time.LocalDate
 
 data class PrisonerAlias (
+  @ApiModelProperty(required = true, value = "First Name", example = "Robert", position = 1)
   val firstName: String,
-  val middleName: String?,
+
+  @ApiModelProperty(value = "Middle names", example = "Trevor", position = 2)
+  val middleNames: String?,
+
+  @ApiModelProperty(required = true, value = "Last name", example = "Lorsen", position = 3)
   val lastName: String,
+
   @Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd")
-  val dob: LocalDate,
+  @ApiModelProperty(required = true, value = "Date of birth", example = "1975-04-02", position = 4)
+  val dateOfBirth: LocalDate,
+
+  @ApiModelProperty(value = "Gender", example = "Male", position = 5)
   val gender: String?,
+
+  @ApiModelProperty(value = "Ethnicity", example = "White : Irish", position = 6)
   val ethnicity: String?
 )
