6ac05a83cbdc641b969b34180317ed04829bf374
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-06-01 09:42:44 +0100

    DT-870 adding initial telemtry event tracking to prisoner search.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/PrisonerIndexListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/PrisonerIndexListener.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.prisonersearch.services
 
 import com.google.gson.Gson
+import com.microsoft.applicationinsights.TelemetryClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
@@ -11,7 +12,8 @@ import uk.gov.justice.digital.hmpps.prisonersearch.services.IndexRequestType.*
 @Service
 class PrisonerIndexListener(
     private val prisonerIndexService: PrisonerIndexService,
-    @Qualifier("gson") private val gson : Gson
+    @Qualifier("gson") private val gson : Gson,
+    private val telemetryClient: TelemetryClient
 ) {
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
@@ -23,14 +25,22 @@ class PrisonerIndexListener(
     val indexRequest = gson.fromJson(requestJson, PrisonerIndexRequest::class.java)
     log.debug("Received message request {}", indexRequest)
 
-    when (indexRequest.requestType) {
-      REBUILD -> {
-        msg.acknowledge()  // ack before processing
-        prisonerIndexService.addIndexRequestToQueue()
+    try {
+      when (indexRequest.requestType) {
+        REBUILD -> {
+          msg.acknowledge()  // ack before processing
+          prisonerIndexService.addIndexRequestToQueue()
+        }
+        OFFENDER_LIST -> indexRequest.pageRequest?.let { prisonerIndexService.addOffendersToBeIndexed(it) }
+        OFFENDER -> indexRequest.prisonerNumber?.let { prisonerIndexService.indexPrisoner(it) }
       }
-      OFFENDER_LIST -> indexRequest.pageRequest?.let { prisonerIndexService.addOffendersToBeIndexed(it) }
-      OFFENDER -> indexRequest.prisonerNumber?.let { prisonerIndexService.indexPrisoner(it) }
+    } catch (e : Exception) {
+      telemetryClient.trackEvent(
+        "POSProcessIndexRequestError",
+        mapOf("message" to e.message),
+        null)
+
+      throw e
     }
   }
-
 }
