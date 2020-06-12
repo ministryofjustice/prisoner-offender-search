3248431a76c54718f57f52851fe5d7f0b809b466
Michael Willis <michael.willis@digital.justice.gov.uk>
2020-06-12 16:51:03 +0100

    DT-918 make testing containers consistent

--- src/test/resources/localstack/setup-es.sh
+++ src/test/resources/localstack/setup-es.sh
@@ -5,5 +5,5 @@ export AWS_ACCESS_KEY_ID=foobar
 export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 
-aws --endpoint-url=http://localhost:4578 es create-elasticsearch-domain --domain-name es1 -o json > /dev/null
+aws --endpoint-url=http://localhost:4578 es create-elasticsearch-domain --domain-name es1 > /dev/null
 echo "ES Configured"
