1a4fb41e05304d312652c7b5f8cff5edf9b850be
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-10-06 14:38:08 +0100

    DT-1121 Give the beans back to localstack

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/config/JmsLocalStackConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/config/JmsLocalStackConfig.kt
@@ -7,6 +7,7 @@ import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
@@ -54,5 +55,17 @@ class JmsLocalStackConfig {
       .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region))
       .withCredentials(AWSStaticCredentialsProvider(AnonymousAWSCredentials()))
       .build()
+
+  @Bean("queueUrl")
+  fun queueUrl(@Qualifier("awsSqsClient") awsSqsClient: AmazonSQS,
+               @Value("\${sqs.queue.name}") queueName: String): String {
+    return awsSqsClient.getQueueUrl(queueName).queueUrl
+  }
+
+  @Bean("indexQueueUrl")
+  fun indexQueueUrl(@Qualifier("awsSqsIndexASyncClient") awsSqsIndexASyncClient: AmazonSQSAsync,
+                    @Value("\${sqs.index.queue.name}") indexQueueName: String): String {
+    return awsSqsIndexASyncClient.getQueueUrl(indexQueueName).queueUrl
+  }
 
 }
