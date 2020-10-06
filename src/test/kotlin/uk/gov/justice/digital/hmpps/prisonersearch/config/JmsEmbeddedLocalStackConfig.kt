09f1405468a7d03c57ef7289953c422861c3bf1b
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-10-06 13:54:02 +0100

    DT-1121 Provide the testcontainer fix in test only!

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/config/JmsEmbeddedLocalStackConfig.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/config/JmsEmbeddedLocalStackConfig.kt
@@ -4,6 +4,10 @@ import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
+import com.amazonaws.services.sqs.model.CreateQueueRequest
+import com.amazonaws.services.sqs.model.QueueAttributeName
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,7 +16,7 @@ import org.testcontainers.containers.localstack.LocalStackContainer
 
 @Configuration
 @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "embedded-localstack")
-open class JmsEmbeddedLocalStackConfig(private val localStackContainer: LocalStackContainer) {
+class JmsEmbeddedLocalStackConfig(private val localStackContainer: LocalStackContainer) {
 
   @Bean
   fun awsSqsClient(): AmazonSQS = AmazonSQSClientBuilder.standard()
@@ -43,4 +47,37 @@ open class JmsEmbeddedLocalStackConfig(private val localStackContainer: LocalSta
     .withEndpointConfiguration(localStackContainer.getEndpointConfiguration(LocalStackContainer.Service.SQS))
     .withCredentials(localStackContainer.defaultCredentialsProvider)
     .build()
+
+
+    @Bean("queueUrl")
+    fun queueUrl(@Qualifier("awsSqsClient") awsSqsClient: AmazonSQS,
+                 @Value("\${sqs.queue.name}") queueName: String,
+                 @Value("\${sqs.dlq.name}") dlqName: String): String {
+        return queueUrlWorkaroundTestcontainers(awsSqsClient, queueName, dlqName)
+    }
+
+    @Bean("indexQueueUrl")
+    fun indexQueueUrl(@Qualifier("awsSqsIndexASyncClient") awsSqsIndexASyncClient: AmazonSQSAsync,
+                      @Value("\${sqs.index.queue.name}") indexQueueName: String,
+                      @Value("\${sqs.index.dlq.name}") indexDlqName: String): String {
+        return queueUrlWorkaroundTestcontainers(awsSqsIndexASyncClient, indexQueueName, indexDlqName)
+    }
+
+    private fun queueUrlWorkaroundTestcontainers(awsSqsClient: AmazonSQS, queueName: String, dlqName: String): String {
+        val queueUrl = awsSqsClient.getQueueUrl(queueName).queueUrl
+        val dlqUrl = awsSqsClient.getQueueUrl(dlqName).queueUrl
+        // This is necessary due to a bug in localstack when running in testcontainers that the redrive policy gets lost
+        val dlqArn = awsSqsClient.getQueueAttributes(dlqUrl, listOf(QueueAttributeName.QueueArn.toString()))
+
+        // the queue should already be created by the setup script - but should reset the redrive policy
+        awsSqsClient.createQueue(
+            CreateQueueRequest(queueName).withAttributes(
+                mapOf(
+                    QueueAttributeName.RedrivePolicy.toString() to
+                            """{"deadLetterTargetArn":"${dlqArn.attributes["QueueArn"]}","maxReceiveCount":"5"}""")
+            ))
+
+        return queueUrl
+    }
+
 }
