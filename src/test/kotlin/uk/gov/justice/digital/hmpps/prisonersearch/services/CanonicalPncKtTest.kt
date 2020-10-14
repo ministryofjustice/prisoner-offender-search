0bdf481d3a113a5183d8e2f2471ee8b436bd5a52
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2020-10-14 17:37:11 +0100

    DT 1186: reformat/tidy tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/CanonicalPncKtTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/CanonicalPncKtTest.kt
@@ -1,121 +1,146 @@
 package uk.gov.justice.digital.hmpps.prisonersearch.services
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class CanonicalPncKtTest {
 
-  @Test
-  internal fun `will convert to canonical form of PNC when it is valid`() {
-    assertThat("2003/1234567a".canonicalPNCNumber()).isEqualTo("2003/1234567A")
-    assertThat("2003/0234567a".canonicalPNCNumber()).isEqualTo("2003/234567A")
-    assertThat("2003/0034567a".canonicalPNCNumber()).isEqualTo("2003/34567A")
-    assertThat("2003/0004567a".canonicalPNCNumber()).isEqualTo("2003/4567A")
-    assertThat("2003/0000567a".canonicalPNCNumber()).isEqualTo("2003/567A")
-    assertThat("2003/0000067a".canonicalPNCNumber()).isEqualTo("2003/67A")
-    assertThat("2003/0000007a".canonicalPNCNumber()).isEqualTo("2003/7A")
-    assertThat("2003/0000000a".canonicalPNCNumber()).isEqualTo("2003/0A")
-    assertThat("03/1234567a".canonicalPNCNumber()).isEqualTo("03/1234567A")
-    assertThat("03/0234567a".canonicalPNCNumber()).isEqualTo("03/234567A")
-    assertThat("03/0034567a".canonicalPNCNumber()).isEqualTo("03/34567A")
-    assertThat("03/0004567a".canonicalPNCNumber()).isEqualTo("03/4567A")
-    assertThat("03/0000567a".canonicalPNCNumber()).isEqualTo("03/567A")
-    assertThat("03/0000067a".canonicalPNCNumber()).isEqualTo("03/67A")
-    assertThat("03/0000007a".canonicalPNCNumber()).isEqualTo("03/7A")
-    assertThat("03/0000000a".canonicalPNCNumber()).isEqualTo("03/0A")
+  @ParameterizedTest
+  @CsvSource(
+    "2003/1234567a,         2003/1234567A",
+    "2003/0234567a,         2003/234567A",
+    "2003/0034567a,         2003/34567A",
+    "2003/0004567a,         2003/4567A",
+    "2003/0000567a,         2003/567A",
+    "2003/0000067a,         2003/67A",
+    "2003/0000007a,         2003/7A",
+    "2003/0000000a,         2003/0A",
+    "03/1234567a,           03/1234567A",
+    "03/0234567a,           03/234567A",
+    "03/0034567a,           03/34567A",
+    "03/0004567a,           03/4567A",
+    "03/0000567a,           03/567A",
+    "03/0000067a,           03/67A",
+    "03/0000007a,           03/7A",
+    "03/0000000a,           03/0A"
+  )
+  internal fun `will convert to canonical form of PNC when it is valid`(input: String, expected: String) {
+    assertThat(input.canonicalPNCNumber()).isEqualTo(expected)
   }
 
-  @Test
-  internal fun `will not convert to canonical form of PNC when it is not valid`() {
-    assertThat("2003/A".canonicalPNCNumber()).isEqualTo("2003/A")
-    assertThat("203/1234567A".canonicalPNCNumber()).isEqualTo("203/1234567A")
-    assertThat("203/1234567".canonicalPNCNumber()).isEqualTo("203/1234567")
-    assertThat("1234567A".canonicalPNCNumber()).isEqualTo("1234567A")
-    assertThat("2013".canonicalPNCNumber()).isEqualTo("2013")
-    assertThat("john smith".canonicalPNCNumber()).isEqualTo("john smith")
-    assertThat("john/smith".canonicalPNCNumber()).isEqualTo("john/smith")
-    assertThat("16/11/2018".canonicalPNCNumber()).isEqualTo("16/11/2018")
-    assertThat("16-11-2018".canonicalPNCNumber()).isEqualTo("16-11-2018")
-    assertThat("111111/11A".canonicalPNCNumber()).isEqualTo("111111/11A")
-    assertThat("SF68/945674U".canonicalPNCNumber()).isEqualTo("SF68/945674U")
-    assertThat("".canonicalPNCNumber()).isEqualTo("")
-    assertThat("2010/BBBBBBBA".canonicalPNCNumber()).isEqualTo("2010/BBBBBBBA")
-    assertThat(" - 20/0009n ".canonicalPNCNumber()).isEqualTo(" - 20/0009n ")
+  @ParameterizedTest
+  @CsvSource(
+    "2003/A,          2003/A",
+    "203/1234567A,    203/1234567A",
+    "203/1234567,     203/1234567",
+    "1234567A,        1234567A",
+    "2013,            2013",
+    "john smith,      john smith",
+    "john/smith,      john/smith",
+    "16/11/2018,      16/11/2018",
+    "16-11-2018,      16-11-2018",
+    "111111/11A,      111111/11A",
+    "SF68/945674U,    SF68/945674U",
+    " , ",
+    "2010/BBBBBBBA,   2010/BBBBBBBA",
+    " - 20/0009n ,    - 20/0009n ",
+  )
+  internal fun `will not convert to canonical form of PNC when it is not valid`(input: String?, expected: String?) {
+    assertThat(input?.canonicalPNCNumber()).isEqualTo(expected)
   }
 
-  @Test
-  internal fun `will convert to canonical form of PNC with short year when it is valid`(){
-    assertThat("2003/1234567a".canonicalPNCNumberShort()).isEqualTo("03/1234567A")
-    assertThat("1993/0234567a".canonicalPNCNumberShort()).isEqualTo("93/234567A")
-    assertThat("2000/0034567a".canonicalPNCNumberShort()).isEqualTo("00/34567A")
-    assertThat("2003/0004567a".canonicalPNCNumberShort()).isEqualTo("03/4567A")
-    assertThat("1993/0000567a".canonicalPNCNumberShort()).isEqualTo("93/567A")
-    assertThat("2000/0000067a".canonicalPNCNumberShort()).isEqualTo("00/67A")
-    assertThat("2003/0000007a".canonicalPNCNumberShort()).isEqualTo("03/7A")
-    assertThat("2000/0000000a".canonicalPNCNumberShort()).isEqualTo("00/0A")
-    assertThat("03/1234567a".canonicalPNCNumberShort()).isEqualTo("03/1234567A")
-    assertThat("93/0234567a".canonicalPNCNumberShort()).isEqualTo("93/234567A")
-    assertThat("00/0034567a".canonicalPNCNumberShort()).isEqualTo("00/34567A")
-    assertThat("03/0004567a".canonicalPNCNumberShort()).isEqualTo("03/4567A")
-    assertThat("93/0000567a".canonicalPNCNumberShort()).isEqualTo("93/567A")
-    assertThat("00/0000067a".canonicalPNCNumberShort()).isEqualTo("00/67A")
-    assertThat("03/0000007a".canonicalPNCNumberShort()).isEqualTo("03/7A")
-    assertThat("93/0000000a".canonicalPNCNumberShort()).isEqualTo("93/0A")
+  @ParameterizedTest
+  @CsvSource(
+    "2003/1234567a,   03/1234567A",
+    "1993/0234567a,   93/234567A",
+    "2000/0034567a,   00/34567A",
+    "2003/0004567a,   03/4567A",
+    "1993/0000567a,   93/567A",
+    "2000/0000067a,   00/67A",
+    "2003/0000007a,   03/7A",
+    "2000/0000000a,   00/0A",
+    "03/1234567a,     03/1234567A",
+    "93/0234567a,     93/234567A",
+    "00/0034567a,     00/34567A",
+    "03/0004567a,     03/4567A",
+    "93/0000567a,     93/567A",
+    "00/0000067a,     00/67A",
+    "03/0000007a,     03/7A",
+    "93/0000000a,     93/0A",
+  )
+  internal fun `will convert to canonical form of PNC with short year when it is valid`(input: String, expected: String) {
+    assertThat(input.canonicalPNCNumberShort()).isEqualTo(expected) }
+
+  @ParameterizedTest
+  @ValueSource
+    (
+    strings = [
+      "2003/A",
+      "203/1234567A",
+      "203/1234567",
+      "1234567A",
+      "2013",
+      "john smith",
+      "john/smith",
+      "16/11/2018",
+      "16-11-2018",
+      "111111/11A",
+      "SF68/945674U",
+      "",
+      "2010/BBBBBBBA",
+      " - 20/0009n "
+    ]
+  )
+  internal fun `will return null and not convert to canonical form of PNC with short year when it is not valid`(input: String?) {
+    assertThat(input?.canonicalPNCNumberShort()).isNull()
   }
 
-  @Test
-  internal fun `will return null and not convert to canonical form of PNC with short year when it is not valid`() {
-    assertThat("2003/A".canonicalPNCNumberShort()).isNull()
-    assertThat("203/1234567A".canonicalPNCNumberShort()).isNull()
-    assertThat("203/1234567".canonicalPNCNumberShort()).isNull()
-    assertThat("1234567A".canonicalPNCNumberShort()).isNull()
-    assertThat("2013".canonicalPNCNumberShort()).isNull()
-    assertThat("john smith".canonicalPNCNumberShort()).isNull()
-    assertThat("john/smith".canonicalPNCNumberShort()).isNull()
-    assertThat("16/11/2018".canonicalPNCNumberShort()).isNull()
-    assertThat("16-11-2018".canonicalPNCNumberShort()).isNull()
-    assertThat("111111/11A".canonicalPNCNumberShort()).isNull()
-    assertThat("SF68/945674U".canonicalPNCNumberShort()).isNull()
-    assertThat("".canonicalPNCNumberShort()).isNull()
-    assertThat("2010/BBBBBBBA".canonicalPNCNumberShort()).isNull()
-    assertThat(" - 20/0009n ".canonicalPNCNumberShort()).isNull()
+  @ParameterizedTest
+  @CsvSource(
+        "2003/1234567a, 2003/1234567A",
+        "1993/0234567a, 1993/234567A",
+        "2000/0034567a, 2000/34567A",
+        "2003/0004567a, 2003/4567A",
+        "1993/0000567a, 1993/567A",
+        "2000/0000067a, 2000/67A",
+        "2003/0000007a, 2003/7A",
+        "1983/0000000a, 1983/0A",
+        "03/1234567a,   2003/1234567A",
+        "93/0234567a,   1993/234567A",
+        "00/0034567a,   2000/34567A",
+        "03/0004567a,   2003/4567A",
+        "93/0000567a,   1993/567A",
+        "00/0000067a,   2000/67A",
+        "03/0000007a,   2003/7A",
+        "83/0000000a,   1983/0A",
+  )
+  internal fun `will convert to canonical form of PNC with long year  when it is valid`(input: String, expected: String) {
+    assertThat(input.canonicalPNCNumberLong()).isEqualTo(expected)
   }
 
-  @Test
-  internal fun `will convert to canonical form of PNC with long year  when it is valid`() {
-    assertThat("2003/1234567a".canonicalPNCNumberLong()).isEqualTo("2003/1234567A")
-    assertThat("1993/0234567a".canonicalPNCNumberLong()).isEqualTo("1993/234567A")
-    assertThat("2000/0034567a".canonicalPNCNumberLong()).isEqualTo("2000/34567A")
-    assertThat("2003/0004567a".canonicalPNCNumberLong()).isEqualTo("2003/4567A")
-    assertThat("1993/0000567a".canonicalPNCNumberLong()).isEqualTo("1993/567A")
-    assertThat("2000/0000067a".canonicalPNCNumberLong()).isEqualTo("2000/67A")
-    assertThat("2003/0000007a".canonicalPNCNumberLong()).isEqualTo("2003/7A")
-    assertThat("1983/0000000a".canonicalPNCNumberLong()).isEqualTo("1983/0A")
-    assertThat("03/1234567a".canonicalPNCNumberLong()).isEqualTo("2003/1234567A")
-    assertThat("93/0234567a".canonicalPNCNumberLong()).isEqualTo("1993/234567A")
-    assertThat("00/0034567a".canonicalPNCNumberLong()).isEqualTo("2000/34567A")
-    assertThat("03/0004567a".canonicalPNCNumberLong()).isEqualTo("2003/4567A")
-    assertThat("93/0000567a".canonicalPNCNumberLong()).isEqualTo("1993/567A")
-    assertThat("00/0000067a".canonicalPNCNumberLong()).isEqualTo("2000/67A")
-    assertThat("03/0000007a".canonicalPNCNumberLong()).isEqualTo("2003/7A")
-    assertThat("83/0000000a".canonicalPNCNumberLong()).isEqualTo("1983/0A")
-  }
-
-  @Test
-  internal fun `will return null and not convert to canonical form of PNC with long year when it is not valid`() {
-    assertThat("2003/A".canonicalPNCNumberLong()).isNull()
-    assertThat("203/1234567A".canonicalPNCNumberLong()).isNull()
-    assertThat("203/1234567".canonicalPNCNumberLong()).isNull()
-    assertThat("1234567A".canonicalPNCNumberLong()).isNull()
-    assertThat("2013".canonicalPNCNumberLong()).isNull()
-    assertThat("john smith".canonicalPNCNumberLong()).isNull()
-    assertThat("john/smith".canonicalPNCNumberLong()).isNull()
-    assertThat("16/11/2018".canonicalPNCNumberLong()).isNull()
-    assertThat("16-11-2018".canonicalPNCNumberLong()).isNull()
-    assertThat("111111/11A".canonicalPNCNumberLong()).isNull()
-    assertThat("SF68/945674U".canonicalPNCNumberLong()).isNull()
-    assertThat("".canonicalPNCNumberLong()).isNull()
-    assertThat("2010/BBBBBBBA".canonicalPNCNumberLong()).isNull()
-    assertThat(" - 20/0009n ".canonicalPNCNumberLong()).isNull()
+  @ParameterizedTest
+  @ValueSource
+    (
+    strings = [
+      "2003/A",
+      "203/1234567A",
+      "203/1234567",
+      "1234567A",
+      "2013",
+      "john smith",
+      "john/smith",
+      "16/11/2018",
+      "16-11-2018",
+      "111111/11A",
+      "SF68/945674U",
+      "",
+      "2010/BBBBBBBA",
+      " - 20/0009n "
+    ]
+  )
+  internal fun `will return null and not convert to canonical form of PNC with long year when it is not valid`(input: String?) {
+    assertThat(input?.canonicalPNCNumberLong()).isNull()
   }
 }
