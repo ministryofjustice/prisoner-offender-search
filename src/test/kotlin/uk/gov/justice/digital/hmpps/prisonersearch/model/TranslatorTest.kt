06ba6bf36922780d4cc6b99f9d6ece110cb86c32
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2020-10-27 15:26:36 +0000

    PAT-1371 adding topup supervision date to elastic (#84)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/TranslatorTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/TranslatorTest.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.prisonersearch.model
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.prisonersearch.services.dto.OffenderBooking
+import uk.gov.justice.digital.hmpps.prisonersearch.services.dto.SentenceDetail
 import java.time.LocalDate
 
 class TranslatorTest {
@@ -18,5 +19,23 @@ class TranslatorTest {
     assertThat(prisoner.lastName).isEqualTo("Bloggs")
     assertThat(prisoner.dateOfBirth).isEqualTo(dateOfBirth)
     assertThat(prisoner.bookingId).isNull()
+  }
+
+  @Test
+  fun `topupSupervisionExpiryDate is present`() {
+
+    val tseDate = LocalDate.of(2021, 5, 15)
+    val prisoner = translate(
+      PrisonerA(),
+      OffenderBooking(
+        "A1234AA",
+        "Fred",
+        "Bloggs",
+        LocalDate.of(1976, 5, 15),
+        false,
+        sentenceDetail = SentenceDetail(topupSupervisionExpiryDate = tseDate)
+      )
+    )
+    assertThat(prisoner.topupSupervisionExpiryDate).isEqualTo(tseDate)
   }
 }
