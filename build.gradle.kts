0bf745a08f34a2076637c20416f75b9ecc8d79eb
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2020-12-14 14:51:46 +0000

    DT-1398: upgraded gradle plugin to 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.6"
-  kotlin("plugin.spring") version "1.4.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.0"
+  kotlin("plugin.spring") version "1.4.21"
 }
 
 configurations {
@@ -22,21 +22,21 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.4.8")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.4.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.4.8")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.1")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.1")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.1")
   implementation("com.nimbusds:nimbus-jose-jwt:8.20")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.apache.commons:commons-lang3:3.11")
   implementation("com.google.code.gson:gson:2.8.6")
-  implementation("com.google.guava:guava:29.0-jre")
+  implementation("com.google.guava:guava:30.0-jre")
 
   implementation("org.springframework:spring-jms")
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.880"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.918"))
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
-  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.880")
+  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.918")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.testcontainers:localstack:1.14.3")
