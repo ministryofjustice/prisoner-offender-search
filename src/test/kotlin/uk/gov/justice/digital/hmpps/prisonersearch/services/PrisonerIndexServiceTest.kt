467090b01708837d97a581a19718f3f7e1c4d8c6
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-03-17 16:51:19 +0000

    DT-1765 Wait for index deletion before recreating

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/PrisonerIndexServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/PrisonerIndexServiceTest.kt
@@ -1,12 +1,24 @@
 package uk.gov.justice.digital.hmpps.prisonersearch.services
 
 import com.microsoft.applicationinsights.TelemetryClient
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.verifyZeroInteractions
 import com.nhaarman.mockitokotlin2.whenever
+import org.apache.http.StatusLine
+import org.elasticsearch.client.Request
+import org.elasticsearch.client.Response
+import org.elasticsearch.client.RestClient
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations
+import org.springframework.data.elasticsearch.core.IndexOperations
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates
 import uk.gov.justice.digital.hmpps.prisonersearch.config.IndexProperties
+import uk.gov.justice.digital.hmpps.prisonersearch.model.SyncIndex
 import uk.gov.justice.digital.hmpps.prisonersearch.repository.PrisonerARepository
 import uk.gov.justice.digital.hmpps.prisonersearch.repository.PrisonerBRepository
 
@@ -31,21 +43,73 @@ class PrisonerIndexServiceTest {
     indexProperties
   )
 
-  @Test
-  fun `indexingComplete - clear messages if index build is complete`() {
-    whenever(indexStatusService.markRebuildComplete()).thenReturn(true)
+  @Nested
+  inner class IndexingComplete {
 
-    prisonerIndexService.indexingComplete(true)
+    @Test
+    fun `clear messages if index build is complete`() {
+      whenever(indexStatusService.markRebuildComplete()).thenReturn(true)
 
-    verify(indexQueueService).clearAllMessages()
+      prisonerIndexService.indexingComplete(true)
+
+      verify(indexQueueService).clearAllMessages()
+    }
+
+    @Test
+    fun `do not clear messages if index build is not complete`() {
+      whenever(indexStatusService.markRebuildComplete()).thenReturn(false)
+
+      prisonerIndexService.indexingComplete(true)
+
+      verifyZeroInteractions(indexQueueService)
+    }
   }
 
-  @Test
-  fun `indexingComplete - do not clear messages if index build is not complete`() {
-    whenever(indexStatusService.markRebuildComplete()).thenReturn(false)
+  @Nested
+  inner class CheckExistsAndReset {
 
-    prisonerIndexService.indexingComplete(true)
+    private val restClient = mock<RestClient>()
+    private val elasticsearchOperations = mock<ElasticsearchOperations>()
+    private val indexOperations = mock<IndexOperations>()
 
-    verifyZeroInteractions(indexQueueService)
+    @BeforeEach
+    fun `mock ES clients`() {
+      whenever(searchClient.lowLevelClient()).thenReturn(restClient)
+      whenever(searchClient.elasticsearchOperations()).thenReturn(elasticsearchOperations)
+      whenever(elasticsearchOperations.indexOps(any<IndexCoordinates>())).thenReturn(indexOperations)
+    }
+
+    @Test
+    fun `waits for index to be gone before trying to recreate it`() {
+      val indexExists = mockIndexExists()
+      val indexMissing = mockIndexMissing()
+
+      whenever(restClient.performRequest(Request("HEAD", "/prisoner-search-a")))
+        .thenReturn(indexExists)
+        .thenReturn(indexExists)
+        .thenReturn(indexMissing)
+
+      prisonerIndexService.checkExistsAndReset(SyncIndex.INDEX_A)
+
+      verify(restClient).performRequest(Request("DELETE", "/prisoner-search-a"))
+      verify(restClient, times(3)).performRequest(Request("HEAD", "/prisoner-search-a"))
+      verify(indexOperations).create()
+    }
+
+    private fun mockIndexMissing(): Response {
+      val checkExistsBadResponse = mock<Response>()
+      val checkExistsBadStatusLine = mock<StatusLine>()
+      whenever(checkExistsBadResponse.statusLine).thenReturn(checkExistsBadStatusLine)
+      whenever(checkExistsBadStatusLine.statusCode).thenReturn(404)
+      return checkExistsBadResponse
+    }
+
+    private fun mockIndexExists(): Response {
+      val checkExistsGoodResponse = mock<Response>()
+      val checkExistsGoodStatusLine = mock<StatusLine>()
+      whenever(checkExistsGoodResponse.statusLine).thenReturn(checkExistsGoodStatusLine)
+      whenever(checkExistsGoodStatusLine.statusCode).thenReturn(200)
+      return checkExistsGoodResponse
+    }
   }
 }
