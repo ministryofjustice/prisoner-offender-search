560a5f4503b37cc53fccd7e377ac3deacd4ed3e2
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-04-01 09:02:04 +0100

    DT-1829 useLatestVersions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.4"
   kotlin("plugin.spring") version "1.4.30"
 }
 
@@ -22,19 +22,19 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.4")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("org.apache.commons:commons-lang3:3.11")
+  implementation("org.apache.commons:commons-lang3:3.12.0")
   implementation("com.google.code.gson:gson:2.8.6")
-  implementation("com.google.guava:guava:30.1-jre")
+  implementation("com.google.guava:guava:30.1.1-jre")
 
   implementation("org.springframework:spring-jms")
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.959"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.989"))
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
-  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.959")
+  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.989")
   implementation("org.awaitility:awaitility-kotlin:4.0.3")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
@@ -42,5 +42,5 @@ dependencies {
   testImplementation("org.testcontainers:elasticsearch:1.15.2")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.1")
-  testImplementation("org.mockito:mockito-inline:3.7.7")
+  testImplementation("org.mockito:mockito-inline:3.8.0")
 }
