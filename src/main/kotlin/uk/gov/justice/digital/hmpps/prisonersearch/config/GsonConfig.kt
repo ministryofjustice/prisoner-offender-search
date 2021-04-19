68dc3e4319eff9227abbd0695092fe5cbb52844e
petergphillips <peter@greenthistle.co.uk>
2021-04-19 17:24:32 +0100

    DT-1893: ✨ Upgrade to java 16 (#135)
    * DT-1893: ✨ Upgrade to java 16
    
    * DT-1893: ✨ Add missing local date time adaptor
    
    * DT-1893: ✨ Switch to jackson configured from spring boot

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/config/GsonConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/config/GsonConfig.kt
@@ -12,7 +12,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.lang.reflect.Type
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @Configuration
 class GsonConfig() {
@@ -21,6 +23,7 @@ class GsonConfig() {
   fun gson(): Gson {
     return GsonBuilder().setPrettyPrinting()
       .registerTypeAdapter(LocalDate::class.java, LocalDateAdapter())
+      .registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeAdapter())
       .create()
   }
 
@@ -31,6 +34,23 @@ class GsonConfig() {
 
     override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): LocalDate? {
       return LocalDate.parse(json?.asJsonPrimitive?.asString)
+    }
+  }
+
+  internal class LocalDateTimeAdapter : JsonSerializer<LocalDateTime?>, JsonDeserializer<LocalDateTime> {
+    override fun serialize(
+      localDateTime: LocalDateTime?,
+      srcType: Type,
+      context: JsonSerializationContext
+    ): JsonElement {
+      return JsonPrimitive(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(localDateTime))
+    }
+
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): LocalDateTime {
+      return LocalDateTime.parse(
+        json.asString,
+        DateTimeFormatter.ISO_LOCAL_DATE_TIME.withLocale(Locale.ENGLISH)
+      )
     }
   }
 }
