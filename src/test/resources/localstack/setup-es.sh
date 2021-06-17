a9c2ba8c9d7d9cef85269e7dde0d26e90185a6fe
Peter Phillips <peter@greenthistle.co.uk>
2021-06-17 12:42:34 +0100

    DT-2209: 🐛 More fixes to docker compose

--- src/test/resources/localstack/setup-es.sh
+++ src/test/resources/localstack/setup-es.sh
@@ -6,4 +6,6 @@ export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 
 aws --endpoint-url=http://localhost:4566 es create-elasticsearch-domain --domain-name es1 > /dev/null
+mkdir /tmp/localstack/es_backup
+chmod -R 777 /tmp/localstack
 echo "ES Configured"
