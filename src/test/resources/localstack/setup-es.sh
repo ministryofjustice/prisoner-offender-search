eae19c7f2f0e3cd1db1e324bb1a2c8c0b04f0e84
petergphillips <peter@greenthistle.co.uk>
2021-06-18 11:39:07 +0100

    DT-2209: 🐛 Document startup and allow override for running in intellij (#160)
    * DT-2209: 🐛 Document startup and allow override for running in intellij
    
    * DT-2209: 🐛 Improve readme

--- src/test/resources/localstack/setup-es.sh
+++ src/test/resources/localstack/setup-es.sh
@@ -8,4 +8,4 @@ export AWS_DEFAULT_REGION=eu-west-2
 aws --endpoint-url=http://localhost:4566 es create-elasticsearch-domain --domain-name es1 > /dev/null
 mkdir /tmp/localstack/es_backup || echo "Failed to create /tmp/localstack/es_backup folder"
 chmod -R 777 /tmp/localstack || echo "Failed to chmod /tmp/localstack folder"
-echo "ES Configured"
+echo "Elasticsearch configured, please now wait for Running on http://0.0.0.0:4571 before connecting"
