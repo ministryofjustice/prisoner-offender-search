3a7386ba6cc9cdabc3439cabcd8b1fdf10f9266e
boydingham <82042696+boydingham@users.noreply.github.com>
2021-07-01 13:16:02 +0100

    DT-2209 - Get prisoner-offender-search running in Docker Desktop for Windows (#166)
    * DT-2209 - Get prisoner-offender-search running in Docker Desktop for Windows
    
    * DT-2209 - Get prisoner-offender-search running in Docker Desktop for Windows - Iteration 2
    
    * DT-2209 - Get prisoner-offender-search running in Docker Desktop for Windows - Iteration 2.a
    
    Co-authored-by: petergphillips <peter@greenthistle.co.uk>

--- src/test/resources/localstack/setup-es.sh
+++ src/test/resources/localstack/setup-es.sh
@@ -6,6 +6,11 @@ export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 
 aws --endpoint-url=http://localhost:4566 es create-elasticsearch-domain --domain-name es1 > /dev/null
-mkdir /tmp/localstack/es_backup || echo "Failed to create /tmp/localstack/es_backup folder"
+if [[ -d /tmp/localstack/es_backup ]]; then
+      echo "/tmp/localstack/es_backup already exists. Attempting to recreate it..."
+      rm -fR /tmp/localstack/es_backup
+      mkdir /tmp/localstack/es_backup || echo "Failed to create /tmp/localstack/es_backup folder"
+      echo "/tmp/localstack/es_backup successfully recreated."
+fi
 chmod -R 777 /tmp/localstack || echo "Failed to chmod /tmp/localstack folder"
-echo "Elasticsearch configured, please now wait for Running on http://0.0.0.0:4571 before connecting"
+echo "Elasticsearch configured. Please wait until '[INFO] Running on http://0.0.0.0:4571' appears before connecting"
