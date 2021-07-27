1d2f63c00613247d4fe3371aa126e8b75da7ce43
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-07-27 08:07:18 +0100

    NN-3569: - minor lib upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,23 +22,23 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.8")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.9")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.apache.commons:commons-lang3:3.12.0")
-  implementation("com.google.code.gson:gson:2.8.6")
+  implementation("com.google.code.gson:gson:2.8.7")
   implementation("com.google.guava:guava:30.1.1-jre")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:0.8.1")
-  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.1020")
+  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.12.32")
   implementation("org.awaitility:awaitility-kotlin:4.1.0")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
-  testImplementation("org.mockito:mockito-inline:3.10.0")
+  testImplementation("org.mockito:mockito-inline:3.11.2")
 }
 
 tasks {
