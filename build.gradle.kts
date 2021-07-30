4f8a6a36cd5c32bb00d2d6fddb42149c76e65d3b
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-07-30 09:04:18 +0100

    DT-2403 - Mitigate CVE-2021-22145 by forcing latest elasticsearch version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.5"
   kotlin("plugin.spring") version "1.5.20"
 }
 
@@ -10,6 +10,9 @@ configurations {
 dependencyCheck {
   suppressionFiles.add("elasticsearch-suppressions.xml")
 }
+
+// Mitigate CVE-2021-22145 - this can be removed once spring-data-elasticsearch's transitive dependency is at this version
+extra["elasticsearch.version"] = "7.13.4"
 
 dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
@@ -22,23 +25,23 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.8")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.10")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.apache.commons:commons-lang3:3.12.0")
-  implementation("com.google.code.gson:gson:2.8.6")
+  implementation("com.google.code.gson:gson:2.8.7")
   implementation("com.google.guava:guava:30.1.1-jre")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:0.8.1")
-  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.1020")
+  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.12.36")
   implementation("org.awaitility:awaitility-kotlin:4.1.0")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
-  testImplementation("org.mockito:mockito-inline:3.10.0")
+  testImplementation("org.mockito:mockito-inline:3.11.2")
 }
 
 tasks {
