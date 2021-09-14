c6f7c5ae1ee38eb437f67b6862e94b6b59186206
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-09-14 12:43:27 +0100

    NN-3648 add alert active and expired data to the index

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/translator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/translator.kt
@@ -9,8 +9,10 @@ fun <P : Prisoner> translate(prisoner: P, ob: OffenderBooking): P {
   prisoner.bookNumber = ob.bookingNo
   prisoner.bookingId = ob.bookingId?.toString()
   prisoner.pncNumber = ob.identifiers?.firstOrNull { i -> i.type == "PNC" }?.value
-  prisoner.pncNumberCanonicalShort = ob.identifiers?.firstOrNull { i -> i.type == "PNC" }?.value?.canonicalPNCNumberShort()
-  prisoner.pncNumberCanonicalLong = ob.identifiers?.firstOrNull { i -> i.type == "PNC" }?.value?.canonicalPNCNumberLong()
+  prisoner.pncNumberCanonicalShort =
+    ob.identifiers?.firstOrNull { i -> i.type == "PNC" }?.value?.canonicalPNCNumberShort()
+  prisoner.pncNumberCanonicalLong =
+    ob.identifiers?.firstOrNull { i -> i.type == "PNC" }?.value?.canonicalPNCNumberLong()
   prisoner.croNumber = ob.identifiers?.firstOrNull { i -> i.type == "CRO" }?.value
 
   prisoner.cellLocation = ob.assignedLivingUnit?.description
@@ -29,15 +31,18 @@ fun <P : Prisoner> translate(prisoner: P, ob: OffenderBooking): P {
   prisoner.middleNames = ob.middleName
   prisoner.lastName = ob.lastName
 
-  prisoner.aliases = ob.aliases?.map { a -> PrisonerAlias(a.firstName, a.middleName, a.lastName, a.dob, a.gender, a.ethnicity) }
-  prisoner.alerts = ob.alerts?.filter { a -> a.active }?.map { a -> PrisonerAlert(a.alertType, a.alertCode) }
+  prisoner.aliases =
+    ob.aliases?.map { a -> PrisonerAlias(a.firstName, a.middleName, a.lastName, a.dob, a.gender, a.ethnicity) }
+  prisoner.alerts =
+    ob.alerts?.filter { a -> a.active }?.map { a -> PrisonerAlert(a.alertType, a.alertCode, a.active, a.expired) }
 
   prisoner.gender = ob.physicalAttributes?.gender
   prisoner.ethnicity = ob.physicalAttributes?.ethnicity
   prisoner.nationality = ob.profileInformation?.firstOrNull { p -> p.type == "NAT" }?.resultValue
   prisoner.religion = ob.profileInformation?.firstOrNull { p -> p.type == "RELF" }?.resultValue
   prisoner.maritalStatus = ob.profileInformation?.firstOrNull { p -> p.type == "MARITAL" }?.resultValue
-  prisoner.youthOffender = ob.profileInformation?.firstOrNull { p -> p.type == "YOUTH" }?.resultValue?.uppercase() == "YES"
+  prisoner.youthOffender =
+    ob.profileInformation?.firstOrNull { p -> p.type == "YOUTH" }?.resultValue?.uppercase() == "YES"
 
   prisoner.sentenceStartDate = ob.sentenceDetail?.sentenceStartDate
   prisoner.confirmedReleaseDate = ob.sentenceDetail?.confirmedReleaseDate
@@ -66,7 +71,8 @@ fun <P : Prisoner> translate(prisoner: P, ob: OffenderBooking): P {
   prisoner.locationDescription = ob.locationDescription
 
   // get the most serious offence for this booking
-  prisoner.mostSeriousOffence = ob.offenceHistory?.firstOrNull { off -> off.mostSerious && off.bookingId == ob.bookingId }?.offenceDescription
+  prisoner.mostSeriousOffence =
+    ob.offenceHistory?.firstOrNull { off -> off.mostSerious && off.bookingId == ob.bookingId }?.offenceDescription
   prisoner.recall = ob.recall
   prisoner.legalStatus = ob.legalStatus
   prisoner.imprisonmentStatus = ob.imprisonmentStatus
