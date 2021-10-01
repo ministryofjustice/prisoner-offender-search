ebb4a141c0152937fbc99132dd1e5164e99c694a
Peter Phillips <peter@greenthistle.co.uk>
2021-10-01 08:08:57 +0100

    DT-2662: 🔒️ Suppress elastic CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,10 +34,10 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.apache.commons:commons-lang3:3.12.0")
   implementation("com.google.code.gson:gson:2.8.8")
-  implementation("com.google.guava:guava:30.1.1-jre")
+  implementation("com.google.guava:guava:31.0.1-jre")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.2")
-  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.12.62")
+  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.12.78")
   implementation("org.awaitility:awaitility-kotlin:4.1.0")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
