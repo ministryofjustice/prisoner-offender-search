53c7b42a70fc90d4e1fddcfb0b3650b600a86bc3
petergphillips <peter@greenthistle.co.uk>
2021-12-14 14:20:15 +0000

    DT-2990: 🔒️ Bump log4j to fix false positives (#230)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0-beta-2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0-beta-3"
   kotlin("plugin.spring") version "1.6.0"
 }
 
