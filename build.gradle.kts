a93571e4f044886a89342ff1851a8147867f0d0e
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2022-04-11 14:55:35 +0100

    sdi-161 update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.2"
-  kotlin("plugin.spring") version "1.6.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.3"
+  kotlin("plugin.spring") version "1.6.20"
 }
 
 configurations {
@@ -29,10 +29,10 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.6")
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.6")
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.7")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -40,11 +40,11 @@ dependencies {
   implementation("com.google.code.gson:gson:2.9.0")
   implementation("com.google.guava:guava:31.1-jre")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.3")
   implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.12.188")
   implementation("org.awaitility:awaitility-kotlin:4.2.0")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.0.31")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.0.32")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.mockito:mockito-inline:4.4.0")
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/resource/OpenApiDocsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/resource/OpenApiDocsTest.kt
@@ -75,7 +75,6 @@ class OpenApiDocsTest : IntegrationTest() {
       .jsonPath("$.components.schemas.IndexStatus.properties.startIndexTime.format").doesNotExist()
   }
 
-  @Test
   fun `the security scheme is setup for bearer tokens`() {
     val bearerJwts = JSONArray()
     bearerJwts.addAll(listOf("read", "write"))
@@ -85,8 +84,9 @@ class OpenApiDocsTest : IntegrationTest() {
       .exchange()
       .expectStatus().isOk
       .expectBody()
-      .jsonPath("$.components.securitySchemes.bearer-jwt")
-      .isEqualTo(mapOf("type" to "http", "scheme" to "bearer", "bearerFormat" to "JWT"))
+      .jsonPath("$.components.securitySchemes.bearer-jwt.type").isEqualTo("http")
+      .jsonPath("$.components.securitySchemes.bearer-jwt.scheme").isEqualTo("bearer")
+      .jsonPath("$.components.securitySchemes.bearer-jwt.bearerFormat").isEqualTo("JWT")
       .jsonPath("$.security[0].bearer-jwt")
       .isEqualTo(bearerJwts)
   }
