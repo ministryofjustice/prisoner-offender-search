8e2670ca327ad763a98c634e46244f05c8f88096
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2022-04-12 10:27:16 +0100

    sdi-161 update dependencies (#251)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/resource/OpenApiDocsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/resource/OpenApiDocsTest.kt
@@ -75,6 +75,7 @@ class OpenApiDocsTest : IntegrationTest() {
       .jsonPath("$.components.schemas.IndexStatus.properties.startIndexTime.format").doesNotExist()
   }
 
+  @Test
   fun `the security scheme is setup for bearer tokens`() {
     val bearerJwts = JSONArray()
     bearerJwts.addAll(listOf("read", "write"))
