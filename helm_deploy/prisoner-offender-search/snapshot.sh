a1c9a98f3df87fc1633e2a67d7384294ced5036f
petergphillips <peter@greenthistle.co.uk>
2022-06-21 15:39:28 +0100

    SDI-238: 🔧 Improve snapshot script (#278)

--- helm_deploy/prisoner-offender-search/snapshot.sh
+++ helm_deploy/prisoner-offender-search/snapshot.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -e
-HTTPIE_SESSION="$HOME/.httpie_session.json"
-HTTPIE_OPTS=("--stream" "--check-status" "--ignore-stdin" "--timeout=600" "--session-read-only=${HTTPIE_SESSION}")
-API_ENDPOINT="${ELASTICSEARCH_PROXY_URL}"
 
-# Setup httpie session
-if ! OUTPUT=$(http --check-status --ignore-stdin --session="${HTTPIE_SESSION}" "${API_ENDPOINT}"); then
+# Check that we can connect to elastic search
+if ! OUTPUT=$(http --check-status --ignore-stdin "${ELASTICSEARCH_PROXY_URL}"); then
   echo -e "\nUnable to talk to elastic search API"
   echo "$OUTPUT"
   exit 1
 fi
 
-http_es() {
-  http "${HTTPIE_OPTS[@]}" "$@"
-}
+http_es() { http --stream --check-status --ignore-stdin --timeout=600 "$@"; }
+
+ENDPOINT_SNAPSHOT_NAMESPACE="$ELASTICSEARCH_PROXY_URL/_snapshot/$NAMESPACE"
+ENDPOINT_LATEST="$ENDPOINT_SNAPSHOT_NAMESPACE/latest"
 
 # Register snapshot repo if not already
-if ! http_es GET "$API_ENDPOINT/_snapshot/$NAMESPACE" &>/dev/null; then
+if ! http_es GET "$ENDPOINT_SNAPSHOT_NAMESPACE" &>/dev/null; then
   # If the snapshot repo with same name as namespace does not exist, create it.
   settings="{
     \"bucket\": \"${BUCKET_NAME}\",
@@ -24,27 +22,27 @@ if ! http_es GET "$API_ENDPOINT/_snapshot/$NAMESPACE" &>/dev/null; then
     \"role_arn\": \"${SNAPSHOT_ROLE_ARN}\",
     \"base_path\": \"${NAMESPACE}\"
   }"
-  http_es --print=Hbh POST "$API_ENDPOINT/_snapshot/$NAMESPACE" \
+  http_es --print=Hbh POST "$ENDPOINT_SNAPSHOT_NAMESPACE" \
     type="s3" \
     settings:="${settings}"
 fi
 
 # Delete latest snapshot if exists
-while http_es GET $API_ENDPOINT/_snapshot/$NAMESPACE/latest &>/dev/null; do
+while http_es GET "$ENDPOINT_LATEST" &>/dev/null; do
   # Delete latest snapshot if exists - the delete will block until completed
   echo -e "\nDeleting existing snapshot"
-  http_es --print=Hbh DELETE "$API_ENDPOINT/_snapshot/$NAMESPACE/latest"
+  http_es --print=Hbh DELETE "$ENDPOINT_LATEST"
   sleep 20
 done
 
 # Take snapshot
 echo -e "\nCreating snapshot"
-http_es --print=Hbh PUT "$API_ENDPOINT/_snapshot/$NAMESPACE/latest"
+http_es --print=Hbh PUT "$ENDPOINT_LATEST"
 
 echo -e "\nWaiting for snapshot to finish"
-while ! http_es GET "$API_ENDPOINT/_snapshot/$NAMESPACE/latest" 2>/dev/null | jq -e '.snapshots[] | select(.state == "SUCCESS")' &>/dev/null; do
+while ! http_es GET "$ENDPOINT_LATEST" 2>/dev/null | jq -e '.snapshots[] | select(.state == "SUCCESS")' &>/dev/null; do
   echo -n "."
   sleep 20
 done
 echo -e "\nSnapshot successful"
-http_es --print=b GET "$API_ENDPOINT/_snapshot/$NAMESPACE/latest"
+http_es --print=b GET "$ENDPOINT_LATEST"
