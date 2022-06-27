26727d5d40da1d51694aca07c2baff8ba5cacb3d
Andy Marke <andy.marke@digital.justice.gov.uk>
2022-06-27 09:32:52 +0100

    SDI-254 Basic skeleton of establishment search

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/resource/PrisonerInEstablishmentResource.kt
@@ -0,0 +1,82 @@
+package uk.gov.justice.digital.hmpps.prisonersearch.resource
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.data.domain.Page
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.prisonersearch.model.Prisoner
+import uk.gov.justice.digital.hmpps.prisonersearch.resource.advice.ErrorResponse
+import uk.gov.justice.digital.hmpps.prisonersearch.services.PrisonerInEstablishmentService
+import uk.gov.justice.digital.hmpps.prisonersearch.services.dto.PrisonerInEstablishmentRequest
+import javax.validation.Valid
+
+@RestController
+@Validated
+@PreAuthorize("hasAnyRole('ROLE_GLOBAL_SEARCH', 'ROLE_PRISONER_SEARCH')")
+class PrisonerInEstablishmentResource(private val searchService: PrisonerInEstablishmentService) {
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  // A hack to allow swagger to determine the response schema which includes generic content
+  abstract class Response : Page<Prisoner>
+
+  @Operation(
+    summary = "Search for a prisoner within a particular establishment",
+    description = """ 
+      This search is optimised for clients that have a simple search term typically containing the prisonser's name
+      or prisoner number. The user typically is certain the prisoner is within the establishment and knows key information 
+      about the prisoner.
+      Requires ROLE_GLOBAL_SEARCH or ROLE_PRISONER_SEARCH role.
+      """,
+    security = [SecurityRequirement(name = "ROLE_GLOBAL_SEARCH"), SecurityRequirement(name = "ROLE_PRISONER_SEARCH")],
+    requestBody = io.swagger.v3.oas.annotations.parameters.RequestBody(
+      content = [
+        Content(
+          mediaType = "application/json",
+          schema = Schema(implementation = PrisonerInEstablishmentRequest::class)
+        )
+      ]
+    ),
+
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Search successfully performed",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = Response::class))]
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Incorrect information provided to perform prisoner match",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))]
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))]
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Incorrect permissions to search for prisoner data",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))]
+      ),
+    ]
+  )
+  @PostMapping("/establishment/{prisonId}/prisoner", produces = [MediaType.APPLICATION_JSON_VALUE])
+  fun search(
+    @PathVariable("prisonId") @Parameter(required = true) prisonId: String,
+    @Valid @RequestBody searchRequest: PrisonerInEstablishmentRequest
+  ): Page<Prisoner> = searchService.search(prisonId, searchRequest)
+}
