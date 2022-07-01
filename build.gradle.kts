208d5f822b2b3cdd3372498fae616bc95d14a047
petergphillips <peter@greenthistle.co.uk>
2022-07-01 15:29:14 +0100

    SDI-257: 🔒️ Suppress false positive for log4j (#290)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.2"
   kotlin("plugin.spring") version "1.7.0"
 }
 
@@ -41,7 +41,7 @@ dependencies {
   implementation("com.google.guava:guava:31.1-jre")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.5")
-  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.12.251")
+  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.12.252")
   implementation("org.awaitility:awaitility-kotlin:4.2.0")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.1")
