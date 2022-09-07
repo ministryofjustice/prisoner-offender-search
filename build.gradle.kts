ba7a68d33f30ae4cfd0a1e7083496a55c5d6d2c1
Peter Phillips <peter@greenthistle.co.uk>
2022-09-07 13:02:10 +0100

    SDI-365: ⬆️  Upgrade plugin to latest to suppress snakeyaml vuln and upgrade app insights

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.3-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.0-beta"
   kotlin("plugin.spring") version "1.7.10"
 }
 
@@ -41,7 +41,7 @@ dependencies {
   implementation("com.google.guava:guava:31.1-jre")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.8")
-  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.12.296")
+  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.12.297")
   implementation("org.awaitility:awaitility-kotlin:4.2.0")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.2")
