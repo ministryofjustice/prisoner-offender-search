ffa0a12f8664d34f5917d3340decc617dd0ce8f8
Mike Halma <mike.halma@digital.justice.gov.uk>
2022-09-21 13:03:12 +0100

    SDI-287 Add method to send prisoner created event

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/HmppsDomainEventEmitter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/HmppsDomainEventEmitter.kt
@@ -8,7 +8,8 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import uk.gov.justice.digital.hmpps.prisonersearch.config.DiffProperties
-import uk.gov.justice.digital.hmpps.prisonersearch.services.HmppsDomainEventEmitter.Companion.EVENT_TYPE
+import uk.gov.justice.digital.hmpps.prisonersearch.services.HmppsDomainEventEmitter.Companion.CREATED_EVENT_TYPE
+import uk.gov.justice.digital.hmpps.prisonersearch.services.HmppsDomainEventEmitter.Companion.UPDATED_EVENT_TYPE
 import uk.gov.justice.digital.hmpps.prisonersearch.services.diff.DiffCategory
 import uk.gov.justice.digital.hmpps.prisonersearch.services.diff.PrisonerDifferences
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
@@ -40,7 +41,7 @@ class HmppsDomainEventEmitter(
         .let { event -> PrisonerUpdatedDomainEvent(event, Instant.now(clock), diffProperties.host) }
         .let { domainEvent ->
           PublishRequest(topicArn, objectMapper.writeValueAsString(domainEvent))
-            .addMessageAttributesEntry("eventType", MessageAttributeValue().withDataType("String").withStringValue(EVENT_TYPE))
+            .addMessageAttributesEntry("eventType", MessageAttributeValue().withDataType("String").withStringValue(UPDATED_EVENT_TYPE))
         }.also { publishRequest ->
           topicSnsClient.publish(publishRequest)
         }
@@ -53,9 +54,26 @@ class HmppsDomainEventEmitter(
       }
   }
 
+  fun emitPrisonerCreatedEvent(offenderNo: String) {
+    runCatching {
+      PrisonerCreatedEvent(offenderNo)
+        .let { event -> PrisonerCreatedDomainEvent(event, Instant.now(clock), diffProperties.host) }
+        .let { domainEvent ->
+          PublishRequest(topicArn, objectMapper.writeValueAsString(domainEvent))
+            .addMessageAttributesEntry("eventType", MessageAttributeValue().withDataType("String").withStringValue(CREATED_EVENT_TYPE))
+        }.also { publishRequest ->
+          topicSnsClient.publish(publishRequest)
+        }
+    }
+      .onFailure {
+        log.error("Failed to send prisoner created event for offenderNo=$offenderNo", it)
+      }
+  }
+
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
-    const val EVENT_TYPE = "prisoner-offender-search.prisoner.updated"
+    const val UPDATED_EVENT_TYPE = "prisoner-offender-search.prisoner.updated"
+    const val CREATED_EVENT_TYPE = "prisoner-offender-search.prisoner.created"
   }
 }
 
@@ -63,6 +81,8 @@ data class PrisonerUpdatedEvent(
   val nomsNumber: String,
   val categoriesChanged: List<DiffCategory>,
 )
+
+data class PrisonerCreatedEvent(val nomsNumber: String)
 
 data class PrisonerUpdatedDomainEvent(
   val additionalInfo: PrisonerUpdatedEvent,
@@ -76,9 +96,28 @@ data class PrisonerUpdatedDomainEvent(
     this(
       additionalInfo = additionalInfo,
       occurredAt = ISO_OFFSET_DATE_TIME.withZone(ZoneId.of("Europe/London")).format(occurredAt),
-      eventType = EVENT_TYPE,
+      eventType = UPDATED_EVENT_TYPE,
       version = 1,
       description = "A prisoner record has been updated",
+      detailUrl = ServletUriComponentsBuilder.fromUriString(host).path("/prisoner/{offenderNo}").buildAndExpand(additionalInfo.nomsNumber).toUri().toString(),
+    )
+}
+
+data class PrisonerCreatedDomainEvent(
+  val additionalInfo: PrisonerCreatedEvent,
+  val occurredAt: String,
+  val eventType: String,
+  val version: Int,
+  val description: String,
+  val detailUrl: String,
+) {
+  constructor(additionalInfo: PrisonerCreatedEvent, occurredAt: Instant, host: String) :
+    this(
+      additionalInfo = additionalInfo,
+      occurredAt = ISO_OFFSET_DATE_TIME.withZone(ZoneId.of("Europe/London")).format(occurredAt),
+      eventType = CREATED_EVENT_TYPE,
+      version = 1,
+      description = "A prisoner record has been created",
       detailUrl = ServletUriComponentsBuilder.fromUriString(host).path("/prisoner/{offenderNo}").buildAndExpand(additionalInfo.nomsNumber).toUri().toString(),
     )
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/HmppsDomainEventsEmitterIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/HmppsDomainEventsEmitterIntTest.kt
@@ -10,7 +10,6 @@ import org.awaitility.kotlin.await
 import org.awaitility.kotlin.matches
 import org.awaitility.kotlin.untilCallTo
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -30,68 +29,81 @@ class HmppsDomainEventsEmitterIntTest : QueueIntegrationTest() {
     }
   }
 
-  @Nested
-  inner class EmitPrisonerDifferenceEvent {
-    @Test
-    fun `sends prisoner differences to the domain topic`() {
-      hmppsDomainEventEmitter.emitPrisonerDifferenceEvent("some_offender", mapOf(IDENTIFIERS to listOf(), LOCATION to listOf()))
+  @Test
+  fun `sends prisoner differences to the domain topic`() {
+    hmppsDomainEventEmitter.emitPrisonerDifferenceEvent("some_offender", mapOf(IDENTIFIERS to listOf(), LOCATION to listOf()))
 
-      await untilCallTo { getNumberOfMessagesCurrentlyOnDomainQueue() } matches { it == 1 }
+    await untilCallTo { getNumberOfMessagesCurrentlyOnDomainQueue() } matches { it == 1 }
 
-      val result = hmppsEventsQueue.sqsClient.receiveMessage(hmppsEventsQueue.queueUrl).messages.first()
-      val message: MsgBody = objectMapper.readValue(result.body)
+    val result = hmppsEventsQueue.sqsClient.receiveMessage(hmppsEventsQueue.queueUrl).messages.first()
+    val message: MsgBody = objectMapper.readValue(result.body)
 
-      assertThatJson(message.Message).node("eventType").isEqualTo("prisoner-offender-search.prisoner.updated")
-      assertThatJson(message.Message).node("version").isEqualTo(1)
-      assertThatJson(message.Message).node("occurredAt").isEqualTo("2022-09-16T11:40:34+01:00")
-      assertThatJson(message.Message).node("detailUrl").isEqualTo("http://localhost:8080/prisoner/some_offender")
-      assertThatJson(message.Message).node("additionalInfo.nomsNumber").isEqualTo("some_offender")
-      assertThatJson(message.Message).node("additionalInfo.categoriesChanged").isArray.containsExactlyInAnyOrder("IDENTIFIERS", "LOCATION")
-    }
+    assertThatJson(message.Message).node("eventType").isEqualTo("prisoner-offender-search.prisoner.updated")
+    assertThatJson(message.Message).node("version").isEqualTo(1)
+    assertThatJson(message.Message).node("occurredAt").isEqualTo("2022-09-16T11:40:34+01:00")
+    assertThatJson(message.Message).node("detailUrl").isEqualTo("http://localhost:8080/prisoner/some_offender")
+    assertThatJson(message.Message).node("additionalInfo.nomsNumber").isEqualTo("some_offender")
+    assertThatJson(message.Message).node("additionalInfo.categoriesChanged").isArray.containsExactlyInAnyOrder("IDENTIFIERS", "LOCATION")
+  }
 
-    @Test
-    fun `e2e - will send prisoner updated event to the domain topic`() {
-      prisonerIndexService.delete("A1239DD")
-      prisonMockServer.stubFor(
-        WireMock.get(WireMock.urlEqualTo("/api/offenders/A1239DD"))
-          .willReturn(
-            WireMock.aResponse()
-              .withHeader("Content-Type", "application/json")
-              .withBody(PrisonerBuilder(prisonerNumber = "A1239DD").toOffenderBooking())
-          )
-      )
-      val message = "/messages/offenderDetailsChanged.json".readResourceAsText().replace("A7089FD", "A1239DD")
+  @Test
+  fun `sends prisoner created events to the domain topic`() {
+    hmppsDomainEventEmitter.emitPrisonerCreatedEvent("some_offender")
 
-      // create the prisoner in ES
-      search(SearchCriteria("A1239DD", null, null), "/results/empty.json")
-      await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
-      eventQueueSqsClient.sendMessage(eventQueueUrl, message)
-      await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
-      await untilCallTo { prisonRequestCountFor("/api/offenders/A1239DD") } matches { it == 1 }
+    await untilCallTo { getNumberOfMessagesCurrentlyOnDomainQueue() } matches { it == 1 }
 
-      // Creating a prisoner does not trigger a prisoner difference (TODO SDI-287 handle new prisoners)
-      assertThat(getNumberOfMessagesCurrentlyOnDomainQueue()).isEqualTo(0)
+    val result = hmppsEventsQueue.sqsClient.receiveMessage(hmppsEventsQueue.queueUrl).messages.first()
+    val message: MsgBody = objectMapper.readValue(result.body)
 
-      // update the prisoner on ES
-      prisonMockServer.stubFor(
-        WireMock.get(WireMock.urlEqualTo("/api/offenders/A1239DD"))
-          .willReturn(
-            WireMock.aResponse()
-              .withHeader("Content-Type", "application/json")
-              .withBody(PrisonerBuilder(prisonerNumber = "A1239DD", firstName = "NEW_NAME").toOffenderBooking())
-          )
-      )
-      eventQueueSqsClient.sendMessage(eventQueueUrl, message)
-      await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
-      await untilCallTo { getNumberOfMessagesCurrentlyOnDomainQueue() } matches { it == 1 }
+    assertThatJson(message.Message).node("eventType").isEqualTo("prisoner-offender-search.prisoner.created")
+    assertThatJson(message.Message).node("version").isEqualTo(1)
+    assertThatJson(message.Message).node("occurredAt").isEqualTo("2022-09-16T11:40:34+01:00")
+    assertThatJson(message.Message).node("detailUrl").isEqualTo("http://localhost:8080/prisoner/some_offender")
+    assertThatJson(message.Message).node("additionalInfo.nomsNumber").isEqualTo("some_offender")
+  }
 
-      // The update should have triggered a prisoner updated domain event
-      val result = hmppsEventsQueue.sqsClient.receiveMessage(hmppsEventsQueue.queueUrl).messages.first()
-      val msgBody: MsgBody = objectMapper.readValue(result.body)
-      assertThatJson(msgBody.Message).node("eventType").isEqualTo("prisoner-offender-search.prisoner.updated")
-      assertThatJson(msgBody.Message).node("additionalInfo.nomsNumber").isEqualTo("A1239DD")
-      assertThatJson(msgBody.Message).node("additionalInfo.categoriesChanged").isArray.containsExactlyInAnyOrder("PERSONAL_DETAILS")
-    }
+  @Test
+  fun `e2e - will send prisoner updated event to the domain topic`() {
+    prisonerIndexService.delete("A1239DD")
+    prisonMockServer.stubFor(
+      WireMock.get(WireMock.urlEqualTo("/api/offenders/A1239DD"))
+        .willReturn(
+          WireMock.aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody(PrisonerBuilder(prisonerNumber = "A1239DD").toOffenderBooking())
+        )
+    )
+    val message = "/messages/offenderDetailsChanged.json".readResourceAsText().replace("A7089FD", "A1239DD")
+
+    // create the prisoner in ES
+    search(SearchCriteria("A1239DD", null, null), "/results/empty.json")
+    await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+    eventQueueSqsClient.sendMessage(eventQueueUrl, message)
+    await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+    await untilCallTo { prisonRequestCountFor("/api/offenders/A1239DD") } matches { it == 1 }
+
+    // Creating a prisoner does not trigger a prisoner difference (TODO SDI-287 handle new prisoners)
+    assertThat(getNumberOfMessagesCurrentlyOnDomainQueue()).isEqualTo(0)
+
+    // update the prisoner on ES
+    prisonMockServer.stubFor(
+      WireMock.get(WireMock.urlEqualTo("/api/offenders/A1239DD"))
+        .willReturn(
+          WireMock.aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody(PrisonerBuilder(prisonerNumber = "A1239DD", firstName = "NEW_NAME").toOffenderBooking())
+        )
+    )
+    eventQueueSqsClient.sendMessage(eventQueueUrl, message)
+    await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+    await untilCallTo { getNumberOfMessagesCurrentlyOnDomainQueue() } matches { it == 1 }
+
+    // The update should have triggered a prisoner updated domain event
+    val result = hmppsEventsQueue.sqsClient.receiveMessage(hmppsEventsQueue.queueUrl).messages.first()
+    val msgBody: MsgBody = objectMapper.readValue(result.body)
+    assertThatJson(msgBody.Message).node("eventType").isEqualTo("prisoner-offender-search.prisoner.updated")
+    assertThatJson(msgBody.Message).node("additionalInfo.nomsNumber").isEqualTo("A1239DD")
+    assertThatJson(msgBody.Message).node("additionalInfo.categoriesChanged").isArray.containsExactlyInAnyOrder("PERSONAL_DETAILS")
   }
 
   companion object {
