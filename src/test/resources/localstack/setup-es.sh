333dd04aaaf071ba9e0018a543f107e11491fb13
steverendell <steve.rendell@syscon.net>
2022-11-01 16:08:54 +0000

    upgrade localstack

--- src/test/resources/localstack/setup-es.sh
+++ src/test/resources/localstack/setup-es.sh
@@ -5,7 +5,7 @@ export AWS_ACCESS_KEY_ID=foobar
 export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 
-aws --endpoint-url=http://localhost:4566 es create-elasticsearch-domain --domain-name es1 >/dev/null
+aws --endpoint-url=http://localhost:4566 es create-elasticsearch-domain --domain-name es01 >/dev/null
 if [[ -d /tmp/localstack/es_backup ]]; then
   echo "/tmp/localstack/es_backup already exists. Attempting to recreate it..."
   rm -fR /tmp/localstack/es_backup
