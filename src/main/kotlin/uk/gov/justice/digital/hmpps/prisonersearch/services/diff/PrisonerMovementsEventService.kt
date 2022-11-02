12907c671107089b54b89ee9658cf5abec7ec33a
Andy Marke <andy.marke@digital.justice.gov.uk>
2022-11-02 08:52:39 +0000

    SDI-301 A few more receive scenarios to show pattern (#356)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/diff/PrisonerMovementsEventService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/diff/PrisonerMovementsEventService.kt
@@ -3,7 +3,11 @@ package uk.gov.justice.digital.hmpps.prisonersearch.services.diff
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.prisonersearch.model.Prisoner
 import uk.gov.justice.digital.hmpps.prisonersearch.services.HmppsDomainEventEmitter
+import uk.gov.justice.digital.hmpps.prisonersearch.services.HmppsDomainEventEmitter.PrisonerReceiveReason.RETURN_FROM_COURT
+import uk.gov.justice.digital.hmpps.prisonersearch.services.HmppsDomainEventEmitter.PrisonerReceiveReason.TEMPORARY_ABSENCE_RETURN
 import uk.gov.justice.digital.hmpps.prisonersearch.services.HmppsDomainEventEmitter.PrisonerReceiveReason.TRANSFERRED
+import uk.gov.justice.digital.hmpps.prisonersearch.services.diff.PossibleMovementChange.MovementChange.CourtReturn
+import uk.gov.justice.digital.hmpps.prisonersearch.services.diff.PossibleMovementChange.MovementChange.TAPReturn
 import uk.gov.justice.digital.hmpps.prisonersearch.services.diff.PossibleMovementChange.MovementChange.TransferIn
 
 @Service
@@ -20,14 +24,34 @@ class PrisonerMovementsEventService(private val domainEventEmitter: HmppsDomainE
         prisonId = movementChange.prisonId,
         fromPrisonId = null /* TODO: get previous prisonId from movements API */
       )
+
+      is CourtReturn -> domainEventEmitter.emitPrisonerReceiveEvent(
+        offenderNo = movementChange.offenderNo,
+        reason = movementChange.reason,
+        prisonId = movementChange.prisonId,
+      )
+
+      is TAPReturn -> domainEventEmitter.emitPrisonerReceiveEvent(
+        offenderNo = movementChange.offenderNo,
+        reason = movementChange.reason,
+        prisonId = movementChange.prisonId,
+      )
     }
   }
 
   private fun calculateMovementChange(previousPrisonerSnapshot: Prisoner?, prisoner: Prisoner): PossibleMovementChange {
     return previousPrisonerSnapshot.let {
-      // TODO: when we do a few more of these we can extract a nice state machine pattern
-      if (previousPrisonerSnapshot?.inOutStatus == "TRN" && prisoner.inOutStatus == "IN") {
-        TransferIn(prisoner.prisonerNumber!!, prisoner.prisonId!!)
+      val prisonerNumber = prisoner.prisonerNumber!!
+      if (prisoner.isTransferIn(previousPrisonerSnapshot)) {
+        TransferIn(prisonerNumber, prisoner.prisonId!!)
+      } else if (prisoner.isCourtReturn(previousPrisonerSnapshot)) {
+        CourtReturn(prisonerNumber, prisoner.prisonId!!)
+      } else if (prisoner.isTransferViaCourt(previousPrisonerSnapshot)) {
+        TransferIn(prisonerNumber, prisoner.prisonId!!)
+      } else if (prisoner.isTAPReturn(previousPrisonerSnapshot)) {
+        TAPReturn(prisonerNumber, prisoner.prisonId!!)
+      } else if (prisoner.isTransferViaTAP(previousPrisonerSnapshot)) {
+        TransferIn(prisonerNumber, prisoner.prisonId!!)
       } else {
         PossibleMovementChange.None
       }
@@ -35,10 +59,41 @@ class PrisonerMovementsEventService(private val domainEventEmitter: HmppsDomainE
   }
 }
 
+private fun Prisoner.isTransferIn(previousPrisonerSnapshot: Prisoner?) =
+  previousPrisonerSnapshot?.inOutStatus == "TRN" && inOutStatus == "IN"
+
+private fun Prisoner.isCourtReturn(previousPrisonerSnapshot: Prisoner?) =
+  previousPrisonerSnapshot?.inOutStatus == "OUT" &&
+    previousPrisonerSnapshot.lastMovementTypeCode == "CRT" &&
+    this.inOutStatus == "IN" &&
+    this.lastMovementTypeCode == "CRT"
+
+private fun Prisoner.isTAPReturn(previousPrisonerSnapshot: Prisoner?) =
+  previousPrisonerSnapshot?.inOutStatus == "OUT" &&
+    previousPrisonerSnapshot.lastMovementTypeCode == "TAP" &&
+    this.inOutStatus == "IN" &&
+    this.lastMovementTypeCode == "TAP"
+
+private fun Prisoner.isTransferViaCourt(previousPrisonerSnapshot: Prisoner?) =
+  previousPrisonerSnapshot?.inOutStatus == "OUT" &&
+    previousPrisonerSnapshot.lastMovementTypeCode == "CRT" &&
+    this.inOutStatus == "IN" &&
+    this.lastMovementTypeCode == "ADM" &&
+    this.lastMovementReasonCode == "TRNCRT"
+
+private fun Prisoner.isTransferViaTAP(previousPrisonerSnapshot: Prisoner?) =
+  previousPrisonerSnapshot?.inOutStatus == "OUT" &&
+    previousPrisonerSnapshot.lastMovementTypeCode == "TAP" &&
+    this.inOutStatus == "IN" &&
+    this.lastMovementTypeCode == "ADM" &&
+    this.lastMovementReasonCode == "TRNTAP"
+
 sealed class PossibleMovementChange {
   sealed class MovementChange(val offenderNo: String, val reason: HmppsDomainEventEmitter.PrisonerReceiveReason) :
     PossibleMovementChange() {
     class TransferIn(offenderNo: String, val prisonId: String) : MovementChange(offenderNo, TRANSFERRED)
+    class CourtReturn(offenderNo: String, val prisonId: String) : MovementChange(offenderNo, RETURN_FROM_COURT)
+    class TAPReturn(offenderNo: String, val prisonId: String) : MovementChange(offenderNo, TEMPORARY_ABSENCE_RETURN)
   }
 
   object None : PossibleMovementChange()
