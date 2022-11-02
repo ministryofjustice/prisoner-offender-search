9fcf4abc96a656bd15ad725b7ff68cffe7bbdfd6
steverendell <steve.rendell@syscon.net>
2022-11-02 10:29:42 +0000

    misc

--- src/test/resources/localstack/setup-es.sh
+++ src/test/resources/localstack/setup-es.sh
@@ -6,12 +6,5 @@ export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 
 aws --endpoint-url=http://localhost:4566 es create-elasticsearch-domain --domain-name es01 >/dev/null
-if [[ -d /tmp/localstack/es_backup ]]; then
-  echo "/tmp/localstack/es_backup already exists. Attempting to recreate it..."
-  rm -fR /tmp/localstack/es_backup
-  mkdir /tmp/localstack/es_backup || echo "Failed to create /tmp/localstack/es_backup folder"
-  echo "/tmp/localstack/es_backup successfully recreated."
-fi
-mkdir -p /tmp/localstack
-chmod -R 777 /tmp/localstack || echo "Failed to chmod /tmp/localstack folder"
+
 echo "Elasticsearch configured. Please wait until '... Active license is now [BASIC]; Security is disabled' appears before connecting"
