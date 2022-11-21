ca469c36693a78de8b6ce65b0fe5880e7e56c196
Andy Marke <andy.marke@digital.justice.gov.uk>
2022-11-21 17:17:02 +0000

    SDI-452 Fix date time format for iep time

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/CurrentIncentive.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/model/CurrentIncentive.kt
@@ -12,8 +12,8 @@ data class CurrentIncentive(
   val level: IncentiveLevel,
   @Schema(description = "Days since last review", example = "120")
   val daysSinceReview: Long,
-  @Field(type = FieldType.Date, format = [DateFormat.date_time])
-  @Schema(required = true, description = "Date time of the incentive", example = "2022-11-10T15:47:24.682335")
+  @Field(type = FieldType.Date, format = [DateFormat.date_hour_minute_second])
+  @Schema(required = true, description = "Date time of the incentive", example = "2022-11-10T15:47:24")
   val dateTime: LocalDateTime,
   @Field(type = FieldType.Date, format = [DateFormat.date])
   @Schema(required = true, description = "Schedule new review date", example = "2022-11-10")
