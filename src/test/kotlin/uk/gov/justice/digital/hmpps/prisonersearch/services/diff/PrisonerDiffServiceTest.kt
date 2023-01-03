a637d8547c94ba3c646ec5db312c5fe3cc199aa6
Andy Marke <andy.marke@digital.justice.gov.uk>
2023-01-03 17:25:25 +0000

    SDI-525 Better diagnostic and decreased transaction scope (#397)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/diff/PrisonerDiffServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/diff/PrisonerDiffServiceTest.kt
@@ -423,13 +423,13 @@ class PrisonerDiffServiceTest {
     }
 
     @Test
-    fun `should raise no-change telemetry if there are no changes`() {
+    fun `should raise no-change found telemetry if there are no changes`() {
       val prisoner1 = Prisoner().apply { pncNumber = "somePnc1" }
       val prisoner2 = Prisoner().apply { pncNumber = "somePnc1" }
 
       prisonerDifferenceService.generateDiffTelemetry(prisoner1, someOffenderBooking(), prisoner2)
 
-      verify(telemetryClient).trackEvent(eq("POSPrisonerUpdatedNoChange"), anyMap(), isNull())
+      verify(telemetryClient).trackEvent(eq("POSPrisonerUpdatedNoChangesFound"), anyMap(), isNull())
     }
 
     @Test
