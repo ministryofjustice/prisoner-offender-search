351e7126aeb339447eacb4de37e927950e5389c2
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2023-02-09 15:40:28 +0000

    SDI-493 Alert if size of index does not match the number of prisoners… (#431)
    * SDI-493 Alert if size of index does not match the number of prisoners in NOMIS
    
    increase timeout
    
    * SDI-493 Alert if size of index does not match the number of prisoners in NOMIS
    
    increase timeout

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/SearchClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/SearchClient.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.prisonersearch.services
 
+import org.apache.http.client.config.RequestConfig
 import org.elasticsearch.action.search.SearchRequest
 import org.elasticsearch.action.search.SearchResponse
 import org.elasticsearch.action.search.SearchScrollRequest
@@ -12,14 +13,19 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations
 import org.springframework.stereotype.Service
 
+private const val ONE_MINUTE = 60000
+
 @Service
 class SearchClient(
   private val elasticsearchClient: RestHighLevelClient,
   @param:Qualifier("elasticsearchOperations") private val elasticsearchOperations: ElasticsearchOperations
 ) {
+  private val requestOptions =
+    RequestOptions.DEFAULT.toBuilder().setRequestConfig(RequestConfig.custom().setSocketTimeout(ONE_MINUTE).build()).build()
+
   fun search(searchRequest: SearchRequest): SearchResponse = elasticsearchClient.search(searchRequest, RequestOptions.DEFAULT)
   fun count(countRequest: CountRequest): CountResponse = elasticsearchClient.count(countRequest, RequestOptions.DEFAULT)
   fun lowLevelClient(): RestClient = elasticsearchClient.lowLevelClient
   fun elasticsearchOperations(): ElasticsearchOperations = elasticsearchOperations
-  fun scroll(searchScrollRequest: SearchScrollRequest): SearchResponse = elasticsearchClient.scroll(searchScrollRequest, RequestOptions.DEFAULT)
+  fun scroll(searchScrollRequest: SearchScrollRequest): SearchResponse = elasticsearchClient.scroll(searchScrollRequest, requestOptions)
 }
