6fc328f6c8631068fb4ee0dee6db37679e0cc346
Peter Phillips <peter@greenthistle.co.uk>
2023-02-10 17:09:14 +0000

    SDI-603: 🐛 Fix stray comma

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/dto/PhysicalDetailRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/services/dto/PhysicalDetailRequest.kt
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class PhysicalDetailRequest(
   @Schema(
     description = "List of prison codes to filter results by",
-    example = "['LEI', 'MDI'],",
+    example = "['LEI', 'MDI']",
     required = true,
   )
   val prisonIds: List<String>? = emptyList(),
