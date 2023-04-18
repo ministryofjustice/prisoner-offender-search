2637144b369d0114cffc4476b76e00205ed6bb99
Peter Phillips <peter@greenthistle.co.uk>
2023-04-18 16:25:29 +0100

    SDIT-747: ⬆️  Upgrade to latest spring security for CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.6-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.6-beta-2"
   kotlin("plugin.spring") version "1.8.10"
 }
 
