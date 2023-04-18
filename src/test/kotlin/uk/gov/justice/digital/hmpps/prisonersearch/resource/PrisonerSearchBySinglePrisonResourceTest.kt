2eb2ffed88b33b889217a4898b7e731d86abc7d7
Michael Willis <mike.willis@digital.cabinet-office.gov.uk>
2023-04-18 15:10:09 +0100

    INC-1228 Provide Find Prisoners on specified incentive level on a prison / location basis

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/resource/PrisonerSearchBySinglePrisonResourceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/resource/PrisonerSearchBySinglePrisonResourceTest.kt
@@ -184,6 +184,7 @@ class PrisonerSearchBySinglePrisonResourceTest : AbstractSearchDataIntegrationTe
     prisonSearch("LNI", "/results/search_results_lni.json")
   }
 
+  @Test
   fun `can perform a match on prisonId and incentive level`() {
     prisonSearchByIncentive("LNI", "STD", "/results/search_results_lni.json")
   }
