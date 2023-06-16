250fc817b713331e5682ee3937e229dd6cacf821
Peter Phillips <peter@greenthistle.co.uk>
2023-06-16 08:31:49 +0100

    SDIT-893: ✨ Cache jwks indefinitely

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonersearch/config/ResourceServerConfiguration.kt
@@ -1,15 +1,22 @@
 package uk.gov.justice.digital.hmpps.prisonersearch.config
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
+@EnableCaching
 class ResourceServerConfiguration : WebSecurityConfigurerAdapter() {
   override fun configure(http: HttpSecurity) {
     http.headers().frameOptions().sameOrigin().and()
@@ -29,4 +36,10 @@ class ResourceServerConfiguration : WebSecurityConfigurerAdapter() {
           .authenticated()
       }.oauth2ResourceServer().jwt().jwtAuthenticationConverter(AuthAwareTokenConverter())
   }
+
+  @Bean
+  fun locallyCachedJwtDecoder(
+    @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") jwkSetUri: String,
+    cacheManager: CacheManager,
+  ): JwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).cache(cacheManager.getCache("jwks")).build()
 }
