aeeecda461a3e5c578b604f1bf592be456581d45
Mike Halma <mike.halma@digital.justice.gov.uk>
2023-07-18 10:50:43 +0100

    SDIT-945 Revert gradle plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.9.3"
   kotlin("plugin.spring") version "1.8.22"
 }
 
