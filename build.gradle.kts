36112e1b4c1d528c484b0700f931b8f2b5d7caad
Mike Halma <mike.halma@digital.justice.gov.uk>
2023-07-24 08:36:37 +0100

    SDIT-945 Downgrade swagger parser

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.3.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.10.0-beta"
   kotlin("plugin.spring") version "1.9.0"
 }
 
@@ -51,7 +51,7 @@ dependencies {
   runtimeOnly("org.postgresql:postgresql:42.6.0")
   runtimeOnly("org.flywaydb:flyway-core")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.16") // Unable to upgrade to 2.1.16 as attempting to use the parser caused a 500 from /v3/api-docs. It seems there's an incorrect dependency on an OAS3.1 type but I couldn't work out which project is wrong - so hopefully this will get sorted by the maintainers in due course.
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.15") // Unable to upgrade to 2.1.16 as attempting to use the parser caused a 500 from /v3/api-docs. It seems there's an incorrect dependency on an OAS3.1 type but I couldn't work out which project is wrong - so hopefully this will get sorted by the maintainers in due course.
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
